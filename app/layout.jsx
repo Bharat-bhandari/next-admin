ed4d10b2b6a83bbb2c7bea48fa2a6c7c3742be39
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-04-30 15:46:15 +0530

    add error boundary'

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,21 +2,24 @@ import "@/assets/styles/globals.css";
 import Nav from "@/components/HomePage/Nav";
 import AuthProvider from "@/components/RestComponents/AuthProvider";
 import Notification from "@/components/RestComponents/Notification";
+import { ErrorBoundaryHandler } from "next/dist/client/components/error-boundary";
 
 const MainLayout = async ({ children }) => {
   return (
     <AuthProvider>
-      <html lang="en">
-        <body
-          suppressHydrationWarning={true}
-          className="text-white font-playfair bg-black2 "
-        >
-          {/* <Nav /> */}
-          <main>{children}</main>
-          <div id="nav-modal"></div>
-          <Notification />
-        </body>
-      </html>
+      <ErrorBoundaryHandler>
+        <html lang="en">
+          <body
+            suppressHydrationWarning={true}
+            className="text-white font-playfair bg-black2 "
+          >
+            {/* <Nav /> */}
+            <main>{children}</main>
+            <div id="nav-modal"></div>
+            <Notification />
+          </body>
+        </html>
+      </ErrorBoundaryHandler>
     </AuthProvider>
   );
 };
