7fd66909d0b5c3645a50abf515549d0e485bfe08
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-04-30 15:58:08 +0530

    will resolve this application error later Let's focus on other things

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,24 +2,21 @@ import "@/assets/styles/globals.css";
 import Nav from "@/components/HomePage/Nav";
 import AuthProvider from "@/components/RestComponents/AuthProvider";
 import Notification from "@/components/RestComponents/Notification";
-import { ErrorBoundaryHandler } from "next/dist/client/components/error-boundary";
 
 const MainLayout = async ({ children }) => {
   return (
     <AuthProvider>
-      <ErrorBoundaryHandler>
-        <html lang="en">
-          <body
-            suppressHydrationWarning={true}
-            className="text-white font-playfair bg-black2 "
-          >
-            {/* <Nav /> */}
-            <main>{children}</main>
-            <div id="nav-modal"></div>
-            <Notification />
-          </body>
-        </html>
-      </ErrorBoundaryHandler>
+      <html lang="en">
+        <body
+          suppressHydrationWarning={true}
+          className="text-white font-playfair bg-black2 "
+        >
+          {/* <Nav /> */}
+          <main>{children}</main>
+          <div id="nav-modal"></div>
+          <Notification />
+        </body>
+      </html>
     </AuthProvider>
   );
 };
