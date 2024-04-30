b00552b86e651edceec78edbdbc2f7cf657a3347
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-04-30 14:46:01 +0530

    rewrite the logic

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,30 +1,6 @@
-"use client";
-
-import { authOptions } from "@/utils/authOptions";
-import { getServerSession } from "next-auth";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React from "react";
 
 const Dashboard = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const fetchSession = async () => {
-      try {
-        const session = await getServerSession(authOptions);
-        console.log("Session in dashboard:", session);
-
-        if (!session?.user?.role === "admin") {
-          router.push("/");
-        }
-      } catch (error) {
-        console.error("Error fetching session:", error);
-      }
-    };
-
-    fetchSession();
-  }, []);
-
   return <div>Dashboard</div>;
 };
 
