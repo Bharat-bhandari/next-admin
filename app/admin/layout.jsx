95fe332c5228821737c9a953f302b76e922554b6
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-04-30 15:04:48 +0530

    let's try with useEffect

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,16 +1,9 @@
+import { redirect } from "next/navigation";
 import AdminSidebar from "@/components/AdminPage/AdminSideBar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
 
 const AdminLayout = async ({ children }) => {
-  const session = await getServerSession(authOptions);
-
-  const isAdmin = session?.user.role === "admin";
-
-  if (!isAdmin) {
-    throw new Error("You need to be an admin");
-  }
-
   return (
     <>
       <div className="font-sans text-black bg-white">
